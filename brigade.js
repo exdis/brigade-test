7991d90125b58bcb568603f217402ae3294e9470
Denis Kolesnikov <exsdis@gmail.com>
2020-03-10 09:49:50 +0300

    Initial commit

---
+++ brigade.js
@@ -0,0 +1,23 @@
+const { events, Job } = require("brigadier")
+
+events.on("exec", e => {
+    console.log(e);
+    var job = new Job("do-nothing", "alpine:3.4")
+    job.tasks = [
+        "echo Hello",
+        "echo World"
+    ]
+
+    job.run();
+});
+
+events.on("simpleevent", e => {
+    console.log(e);
+    var job = new Job("do-nothing", "alpine:3.4")
+    job.tasks = [
+        "echo Hello",
+        "echo World from webhook"
+    ]
+
+    job.run();
+});
